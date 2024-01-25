38b99a3bc0e005e0a54bd27973c2ecf2dc35dc54
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-01-25 12:29:20 +0800

    feat: connect to mongodb

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,17 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = chai.assert
+const server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
-  
-});
+suite('Functional Tests', function () {
+  suite('POST /api/issues/:project', () => {
+    test('POST with all required fields', (done) => {
+      const input = []
+      const expected = {}
+
+      chai.request(server).post('/api/issues/').body
+    })
+  })
+})
